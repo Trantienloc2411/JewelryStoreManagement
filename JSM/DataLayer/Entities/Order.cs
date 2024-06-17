03e7246fe87dfe6ed9fc590beb1471891d77bba0
Tran Tien Loc <ttloc2411@gmail.com>
2024-06-17 12:00:49 +0700

    Update DB

--- JSM/DataLayer/Entities/Order.cs
+++ JSM/DataLayer/Entities/Order.cs
@@ -9,7 +9,7 @@ namespace DataLayer.Entities
         public DateTime OrderDate { get; set; }
         public double Discount { get; set; }
         public Types Type { get; set; }
-        public Guid PromotionCode { get; set; }   
+        public string PromotionCode { get; set; }   
         public int AccumulatedPoint { get; set; }
         public int CounterId { get; set; }
         public int PaymentId { get; set; }
--- JSM/JSMServices/IServices/IPromotionService.cs
+++ JSM/JSMServices/IServices/IPromotionService.cs
@@ -1,6 +1,9 @@
+using JSMServices.ViewModels.PromotionViewModel;
+
 namespace JSMServices.IServices;
 
 public interface IPromotionService
 {
-    
+    Task AddNewPromotion(CreatePromotionViewModel viewModel);
+    Task UpdatePromotion();
 }
